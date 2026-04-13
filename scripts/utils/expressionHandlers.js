ac85217e6444cbd415f0c3504e6f1268dee3a5db
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-04-13 11:24:39 +0530

    Refactor: Implement new tokenize function and add helper functions.

---
+++ scripts/utils/expressionHandlers.js
@@ -0,0 +1,19 @@
+import { operators } from "../modules/operatorReference.js";
+
+function handleUnaryOperators(tokens){
+    for(let i = 0 ; i < tokens.length ; i++){
+        if ((tokens[i] === "-" || tokens[i] === "+" || tokens[i] === "!") && (i === 0 || operators[tokens[i-1]]?.precedence !== undefined || tokens[i - 1] === "|")) {
+            if(tokens[i] === "!"){
+                throw new SyntaxError("Wrong placement of factorial");
+            }
+            else if(tokens[i] === "-"){
+                tokens[i] = "UM";
+            }
+            else{
+                tokens[i] = "";
+            }
+        }
+    }
+}
+
+export { handleUnaryOperators };
